f714cbee72a6e1ed23bf3f61e21c7263ab1f1025
bzenatti <73440868+bzenatti@users.noreply.github.com>
2022-10-19 21:42:19 -0300

    Add files via upload

--- Código/Main.cpp
+++ Código/Main.cpp
@@ -4,10 +4,12 @@
 
 using namespace std;
 
+bool ehMaiusculo(char c);
+
 class Simbolo {
     
-    vector <Simbolo> conexoes;                                          //Quais conexoes possui
-    char nome;                                                          //Nome do simbolo
+    vector <Simbolo> conexoes;                                           //Quais conexoes possui
+    char nome;                                                           //Nome do simbolo
 
 public:
 
@@ -38,42 +40,45 @@ public:
 
 };
 
+// int Simbolo::maximo = 0;
+
 int main() {
 
-    int numleituras, k = 0;
-    vector <Simbolo> alfabeto;
-    string simb, lixo, conexao;
+    int numleituras, k = 0, tamPalavra = 0, qtdConex = 0;
+    vector <Simbolo> linguagem;
+    string simb, lixo, conexao, entrada;
     vector <Simbolo> :: iterator posicao;
 
     cin >> numleituras;
 
     for(int i = 0; i < numleituras; i++){
         
-        cin >> simb >> lixo >> conexao;                                 //Le o simbolo, o lixo(->) e a conexao
+        cin >> simb >> lixo >> conexao;                                     //Le o simbolo, o lixo(->) e a conexao
         //cout << simb << " " << conexao << endl;
 
-        if(simb[0] >= 65 && simb[0] <= 90) {                            //Verifica se eh letra maiuscula (obedece a chomsky)
+        if(ehMaiusculo(simb[0])) {                                          //Verifica se eh letra maiuscula (obedece a chomsky)
 
         bool jaExiste = false;
 
-            for(int j = 0; j < alfabeto.size(); j++) {
+            for(int j = 0; j < linguagem.size(); j++) {
 
-                if(simb[0] == alfabeto[j].getNome()) {
+                if(simb[0] == linguagem[j].getNome()) {
 
                     jaExiste = true;
 
                     // cout << "ja existe" << endl;
 
-                    if(conexao[0] >= 65 && conexao[0] <= 90) {             //Caso a conexao nao seja terminal
+                    if(ehMaiusculo(conexao[0])) {                          //Caso a conexao nao seja terminal
 
-                        alfabeto[j].setConexoes(conexao[0]);               //Seta mais conexoes a esse simbolo
-                        alfabeto[j].setConexoes(conexao[1]);
+                        linguagem[j].setConexoes(conexao[0]);               //Seta mais conexoes a esse simbolo
+                        linguagem[j].setConexoes(conexao[1]);
+                        
 
                         // cout << "nao eh terminal" << endl;
                     }
 
                     else {                                                 //Caso seja terminal
-                        alfabeto[j].setConexoes(conexao[0]);               //Seta mais uma conexao a esse simbolo
+                        linguagem[j].setConexoes(conexao[0]);               //Seta mais uma conexao a esse simbolo
 
                         // cout << "eh terminal" << endl;
                     }
@@ -85,24 +90,24 @@ int main() {
 
                 // cout << "nao existe" << endl;
 
-                alfabeto.push_back(Simbolo(simb[0]));                      //Coloca no final do vector
+                linguagem.push_back(Simbolo(simb[0]));                      //Coloca no final do vector
 
                 /*
                 se esse cara de iterador funcionar, o trem todo funciona!
                 */
 
                 if(i == 0) {                                               //Inicializa o iterador em um ponto que existe
-                    posicao = alfabeto.begin();
+                    posicao = linguagem.begin();
                     k++;
                 }
                 else {
-                    posicao = alfabeto.begin() + k;
+                    posicao = linguagem.begin() + k;
                     k++;
                 }
 
                 // cout << "colocou " << (*posicao).getNome() << endl;
 
-                if(conexao[0] >= 65 && conexao[0] <= 90) {                //Caso a conexao nao seja terminal
+                if(ehMaiusculo(conexao[0])) {                             //Caso a conexao nao seja terminal
                     (*posicao).setConexoes(conexao[0]);                   //Seta mais conexoes a esse simbolo
                     (*posicao).setConexoes(conexao[1]);
 
@@ -122,20 +127,74 @@ int main() {
         // cout << "finaliza o loop" << endl;
     }
 
-    for(int j = 0; j < alfabeto.size(); j++){                             //Teste do alfabeto
+    /* for(int j = 0; j < linguagem.size(); j++){                             //Teste do linguagem
 
-        cout << '\n' << alfabeto[j].getNome() << " - ";
+        cout << '\n' << linguagem[j].getNome() << " - ";
 
-        for(int i = 0; i < alfabeto[j].getQntConexoes(); i++){
+        for(int i = 0; i < linguagem[j].getQntConexoes(); i++){
 
-            cout << alfabeto[j].getConexoes(i).getNome();
+            cout << linguagem[j].getConexoes(i).getNome();
 
         }
 
         cout << endl;
+    } */
+
+    
+    /* Checa o maximo de conexoes possivel */
+    for(int i = 0;i < linguagem.size();i++) {
+        qtdConex += linguagem[i].getQntConexoes();
     }
 
+
+
+    while(1) {
+        
+        cin >> entrada;
+
+        tamPalavra = entrada.length();
+
+        if(tamPalavra == 0 || entrada == "*"){
+            break;
+        }
+
+        /*Inicio da logica*/
+
+        char table[tamPalavra][tamPalavra][qtdConex] = {};
+
+        for(int i = 0; i < tamPalavra; i++) {
+
+            int aux = 0;
+            for(int j = 0; j < linguagem.size(); j++){
+
+                for(int k = 0; k < linguagem[j].getQntConexoes(); k++){
+
+                    if(entrada[i] == linguagem[j].getConexoes(k).getNome()){
+
+                        table[i][i][aux] = linguagem[j].getNome();
+                        
+                        cout << table[i][i][aux] << endl;
+                        aux++;
+                    }
+                }
+            }
+        }
+        
+
+    }
+
+    return 0;
+
 }
+
+bool ehMaiusculo(char c) {
+    if(c >= 65 && c <= 90) 
+        return true;
+    else 
+        return false;
+}
+
+//Testing
 
 /*
 7
@@ -146,6 +205,7 @@ X -> RB
 R -> AB
 A -> a
 B -> b
+aabb
 */
 
 /*
